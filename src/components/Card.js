93259cbc1ccc90b61351f0c1ad91439b4bcd6dbf
dewaangga <devaangga53@gmail.com>
2022-02-03 21:57:09 +0800

    props childern card

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+
+function Card(props) {
+  //cara membuat komponen pembungkus
+  //return <div className="card">{props.children}</div>
+
+  //cara membuat komponen pembungkus tetapi bisa ditambahkan style cssnya
+  //di dalam clas harus ada spasi nya contoh'card(spasi)' baru mau jalan
+  const classed = 'card ' + props.className;
+  return <div className={classed}>{props.children}</div>;
+}
+
+export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 //membuat fungsi prop agar kita bisa menggunakan komponen dengan data yang berbeda
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* import komponen props di dalam komponent  */}
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
